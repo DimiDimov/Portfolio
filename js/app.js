1e3862bd11a41ac89995cc7857d218de79385512
DimiDimov <ddimov@uw.edu>
2015-11-23 21:52:17 -0800

    last one

--- js/app.js
+++ js/app.js
@@ -28,8 +28,9 @@ var myApp = angular.module('myApp', ['ui.router'])
   		$scope.homePicture2 = data[1].PICTURE;
   		$scope.welcomeParagraph3 = data[2].WELCOME;
   		$scope.homePicture3 = data[2].PICTURE;
+  		return false;
   });
-  return false;
+  
 })
 
 // About page controller: define $scope.about as a string
@@ -37,13 +38,13 @@ var myApp = angular.module('myApp', ['ui.router'])
   $.getJSON("data/aboutData.json").then(function (data) {
   		$scope.introParagraphs = data[0].PARAGRAPH;
   		$scope.aboutPicture = data[0].PICTURE;
-  });
-  return false;
+  		return false;
+  });  
 })
 
 // Content controller: define $scope.url as an image
 .controller('ContentController', function($scope){
-
+	alert("for some reason the first image won't load even though it works on my computer and is on github correctly, also sometimes you need to hit tabs 2 times for them to work.");
   $.getJSON("data/projectsData.json").then(function (data) {
   		$scope.exampleOne = data[0].PARAGRAPH;
   		$scope.exampleOnePic = data[0].PICTURE;
@@ -53,6 +54,8 @@ var myApp = angular.module('myApp', ['ui.router'])
   		$scope.exampleThreePic = data[2].PICTURE;
     });
   $scope.linkToPolice = "http://students.washington.edu/ddimov/info343/2/police-shooting/";
+  $scope.linkToDawg = "http://students.washington.edu/ddimov/info343/c2/dawg-coffee/";
+  $scope.linkToReview = "http://students.washington.edu/ddimov/info343/CRUD/CRUDINFO343/";
   return false;
 })
 
